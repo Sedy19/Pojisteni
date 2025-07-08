dc4b0bfaa846f31628c12e0929e37924c326db5c
Sedy19 <zdesedlackova@icloud.com>
2025-07-08 11:08:08 +0200

    Třída reprezentující pojištěnce, bere do konstruktoru jako parametr jméno, příjmení, telefonní číslo a věk. Vypisuje pomocí toString textovou reprezentaci pojištěného. Uživatelské rozhraní vypisuje hlavní nabídku do konzole a bere zpět volbu další akce. Třída Main bere z uživatelského rozhraní metodu pro vykreslení hlavní nabídky a výběr volby.

---
+++ src/cz/itnetwork/Main.java
@@ -0,0 +1,10 @@
+package cz.itnetwork;
+
+public class Main {
+    public static void main(String[] args) {
+        UzivatelskeRozhrani uzivatelskeRozhrani = new UzivatelskeRozhrani();
+        uzivatelskeRozhrani.vykresliNabidku();
+
+
+    }
+}
